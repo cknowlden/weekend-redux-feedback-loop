99bdc709f31aeee795067d54ac300656bad871d7
Cali Knowlden <calbearnc@gmail.com>
2024-02-10 00:05:34 -0600

    added comment box in comment component

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Comments() {
-  return <div>Comments</div>;
+  const [comment, setComment] = useState('');
+  return (
+    <div data-testid="input">
+      <h4>Supported?</h4>
+      <textarea
+        value={comment}
+        onChange={(event) =>
+          setComment((v) =>
+            event.target.validity.valid ? event.target.value : v
+          )
+        }
+      />
+    </div>
+  );
 }
-
 export default Comments;
--- src/components/Supported/Supported.jsx
+++ src/components/Supported/Supported.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Supported() {
-  return <div>Supported</div>;
+  const [scoreSupported, setScoreSupported] = useState('');
+  return (
+    <div data-testid="input">
+      <h4>Supported?</h4>
+      <input
+        type="number"
+        min={0}
+        max={5}
+        value={scoreSupported}
+        onChange={(event) =>
+          setScoreSupported((v) =>
+            event.target.validity.valid ? event.target.value : v
+          )
+        }
+      />
+    </div>
+  );
 }
 
 export default Supported;
